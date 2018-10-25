671620d94ad9100449c4b2727696bd5446ff0001
vidhi1293 <35034821+vidhi1293@users.noreply.github.com>
2018-10-25 19:56:41 +0530

    dfg

--- src/app/admin/instructor-course-form/instructor-course-form.component.ts
+++ src/app/admin/instructor-course-form/instructor-course-form.component.ts
@@ -8,56 +8,66 @@ declare var CKEDITOR, Dropzone, $: any;
 })
 export class InstructorCourseFormComponent implements OnInit {
 
+  CourseFormList;
+  secondform;
+
   constructor() { }
 
   ngOnInit() {
+this.secondform = false;
+    var item = { 'Name': ''};
+    this.CourseFormList = [];
+    this.CourseFormList.push(item);
 
     //pre-next btn
-    var divs = $('.mydivs>div');
-    var now = 0; // currently shown div
-    divs.hide().first().show();
-    $("button[name=next]").click(function (e) {
-      divs.eq(now).hide();
-      now = (now + 1 < divs.length) ? now + 1 : 0;
-      divs.eq(now).show(); // show next
-    });
-    $("button[name=prev]").click(function (e) {
-      divs.eq(now).hide();
-      now = (now > 0) ? now - 1 : divs.length - 1;
-      divs.eq(now).show(); // or .css('display','block');
-      //console.log(divs.length, now);
-    });
+    // var divs = $('.mydivs>div');
+    // var now = 0; // currently shown div
+    // divs.hide().first().show();
+    // $("button[name=next]").click(function (e) { 
+    //   divs.eq(now).hide();
+    //   now = (now + 1 < divs.length) ? now + 1 : 0;
+    //   divs.eq(now).show(); // show next
+      
+    // });
+    // $("button[name=prev]").click(function (e) {
+    //   divs.eq(now).hide();
+    //   now = (now > 0) ? now - 1 : divs.length - 1;
+    //   divs.eq(now).show(); // or .css('display','block');
+      
+      
+    // });
     //pre-next btn 
 
+
     //add more
-    var regex = /^(.+?)(\d+)$/i;
-    var cloneIndex = $(".clonedInput").length + 1;
+    // var regex = /^(.+?)(\d+)$/i;
+    // var cloneIndex = $(".clonedInput").length + 1;
 
-    function clone() {
-      $(".clonedInput:first-child").clone()
-        .appendTo(".course_forum_block")
-        .attr("id", "clonedInput" + cloneIndex)
-        .find("*")
-        .each(function () {
-          var id = this.id || "";
-          var match = id.match(regex) || [];
-          if (match.length == 3) {
-            this.id = match[1] + (cloneIndex);
-            this.name = match[1] + (cloneIndex);
+    // function clone() {
+    //   $(".clonedInput:first-child").clone()
+    //     .appendTo(".course_forum_block")
+    //     .attr("id", "clonedInput" + cloneIndex)
+    //     .find("*")
+    //     .each(function () {
+    //       var id = this.id || "";
+    //       var match = id.match(regex) || [];
+    //       if (match.length == 3) {
+    //         this.id = match[1] + (cloneIndex);
+    //         this.name = match[1] + (cloneIndex);
 
-          }
-        })
-        .on('click', 'button.clone', clone)
-        .on('click', 'button.remove', remove);
+    //       }
+    //     })
+    //     .on('click', 'button.clone', clone)
+    //     .on('click', 'button.remove', remove);
 
-      cloneIndex++;
-    }
-    function remove() {
-      $(".clonedInput:last-child").remove();
-    }
-    $("button.clone").on("click", clone);
+    //   cloneIndex++;
+    // }
+    // function remove() {
+    //   $(".clonedInput:last-child").remove();
+    // }
+    // $("button.clone").on("click", clone);
 
-    $("button.remove").on("click", remove);
+    // $("button.remove").on("click", remove);
     //add more
 
     // function remove(){
@@ -76,8 +86,7 @@ export class InstructorCourseFormComponent implements OnInit {
       extraAllowedContent: 'span;ul;li;table;td;style;*[id];*(*);*{*}'
       //extraAllowedContent: 'style;*[id,rel](*){*}
     });
-
-    CKEDITOR.replace('EmailBody1', {
+    CKEDITOR.replace('EmailBodyW', {
       height: '100',
       resize_enabled: 'false',
       resize_maxHeight: '100',
@@ -87,8 +96,7 @@ export class InstructorCourseFormComponent implements OnInit {
       extraAllowedContent: 'span;ul;li;table;td;style;*[id];*(*);*{*}'
       //extraAllowedContent: 'style;*[id,rel](*){*}
     });
-
-    CKEDITOR.replace('EmailBody2', {
+    CKEDITOR.replace('EmailBodyT', {
       height: '100',
       resize_enabled: 'false',
       resize_maxHeight: '100',
@@ -98,19 +106,7 @@ export class InstructorCourseFormComponent implements OnInit {
       extraAllowedContent: 'span;ul;li;table;td;style;*[id];*(*);*{*}'
       //extraAllowedContent: 'style;*[id,rel](*){*}
     });
-
-    CKEDITOR.replace('EmailBody3', {
-      height: '100',
-      resize_enabled: 'false',
-      resize_maxHeight: '100',
-      resize_maxWidth: '948',
-      resize_minHeight: '100',
-      resize_minWidth: '948',
-      extraAllowedContent: 'span;ul;li;table;td;style;*[id];*(*);*{*}'
-      //extraAllowedContent: 'style;*[id,rel](*){*}
-    });
-
-    CKEDITOR.replace('EmailBody4', {
+    CKEDITOR.replace('EmailBodyM', {
       height: '100',
       resize_enabled: 'false',
       resize_maxHeight: '100',
@@ -140,17 +136,56 @@ export class InstructorCourseFormComponent implements OnInit {
       }
     });
 
-    $('.alert-close').on('click', function (c) { debugger
-      $(this).parent().fadeOut('slow', function (c) {
-      });
-    });
+    // $('.alert-close').on('click', function (c) { debugger
+    //   $(this).parent().fadeOut('slow', function (c) {
+    //   });
+    // });
 
   }
 
-  // close(){
-  //   $(this).parent().fadeOut('slow', function (c) {
-  //   });
-  // }
+  AddCourseForm(){ 
+    var item = { 'Name':'' };
+    this.CourseFormList.splice(this.CourseFormList.length, 0, item);
+    var index = this.CourseFormList.length;
+    index = 'EmailBody'+index;
+    CKEDITOR.replace(index, {
+      height: '100',
+      resize_enabled: 'false',
+      resize_maxHeight: '100',
+      resize_maxWidth: '948',
+      resize_minHeight: '100',
+      resize_minWidth: '948',
+      extraAllowedContent: 'span;ul;li;table;td;style;*[id];*(*);*{*}'
+      //extraAllowedContent: 'style;*[id,rel](*){*}
+    });
+    // if (this.CourseFormList.length <= index + 1) {
+      
+    // }
+    }
+  
+    DeleteCourseForm(item){ 
+    var index = this.CourseFormList.indexOf(item);	
+    this.CourseFormList.splice(index, 1);		
+    }
 
+    next_btn(){
+      this.secondform = true;
+      for(var index=1; index<=this.CourseFormList.length; index++){
+        CKEDITOR.replace('EmailBody'+index, {
+          height: '100',
+          resize_enabled: 'false',
+          resize_maxHeight: '100',
+          resize_maxWidth: '948',
+          resize_minHeight: '100',
+          resize_minWidth: '948',
+          extraAllowedContent: 'span;ul;li;table;td;style;*[id];*(*);*{*}'
+          //extraAllowedContent: 'style;*[id,rel](*){*}
+        });
+      }
+    } 
+
+    back_btn(){
+      this.secondform = false;
+    }
 
 }
