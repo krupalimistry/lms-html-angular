09bf08787d1ded259ff925a184fa458090cba648
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2018-10-26 19:48:17 +0530

    AC

--- src/app/admin/link-list/link-list.component.ts
+++ src/app/admin/link-list/link-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-link-list',
@@ -10,6 +11,27 @@ export class LinkListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+      var owl = $('.owl-carousel');
+      owl.owlCarousel({
+        margin: 10,
+        nav: true,
+        dots: false,
+        loop: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 5
+          }
+        }
+      })
+
+
   }
 
 }
