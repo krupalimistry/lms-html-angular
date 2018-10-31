69e885c5f3bb6911e0d67767b7efa35e1685e573
vidhi1293 <35034821+vidhi1293@users.noreply.github.com>
2018-10-31 19:34:47 +0530

    dfgd

---
+++ logs/log-2018-10-31.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-10-31 16:45:50 --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() at line no 1894 in C:\wamp64\www\lms-html-angular\api\system\libraries\Email.php
+ERROR - 2018-10-31 16:48:57 --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() at line no 1894 in C:\wamp64\www\lms-html-angular\api\system\libraries\Email.php
+ERROR - 2018-10-31 16:50:27 --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() at line no 1894 in C:\wamp64\www\lms-html-angular\api\system\libraries\Email.php
+ERROR - 2018-10-31 17:04:04 --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() at line no 1894 in C:\wamp64\www\lms-html-angular\api\system\libraries\Email.php
