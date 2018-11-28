ac6994773a24ea5a08e97ac99ecc1ce845b5384f
uttambhut222 <39264925+uttambhut222@users.noreply.github.com>
2018-11-28 13:06:46 +0530

    fgb

--- logs/log-2018-10-18.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2018-10-18 13:57:39 --> Undefined index: InstracterId at line no 12 in C:\wamp64\www\lms-html-angular\api\application\models\Course_model.php
-ERROR - 2018-10-18 13:57:39 --> Query error: Column 'InstracterId' cannot be null - Invalid query: INSERT INTO `tblcoursepayment` (`LernerId`, `InstracterId`, `CourseId`, `TotalAmount`, `PaidAmount`, `CreatedBy`, `UpdatedBy`) VALUES ('1', NULL, 1, 1, 1, '1', '1')
-ERROR - 2018-10-18 13:57:39 --> Database error! Error Code [1048] Error: Column 'InstracterId' cannot be null at line no 40 in C:\wamp64\www\lms-html-angular\api\application\models\Course_model.php
